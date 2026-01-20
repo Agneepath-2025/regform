3448a209203642492db28039e5619f1fe95aa106
Cat-logo-in-github <parshatwork@gmail.com>
2026-01-20 17:21:46 +0400

    Push to main ready 👍

--- app/utils/forms/schema.ts
+++ app/utils/forms/schema.ts
@@ -45,8 +45,7 @@ export const sports: Record<string, string> = {
     Volleyball_Women: "Volleyball (Women)",
     Table_Tennis_Men: "Table Tennis (Men)",
     Table_Tennis_Women: "Table Tennis (Women)",
-    Squash_Men: "Squash (Men)",
-    Squash_Women: "Squash (Women)",
+    Squash: "Squash",
     Swimming_Men: "Swimming (Men)",
     Swimming_Women: "Swimming (Women)",
     Ball_Pool_Mixed: "8 Ball Pool (Mixed)",
@@ -171,8 +170,7 @@ export const sportField = z.object({
         "Volleyball (Women)",
         "Table Tennis (Men)",
         "Table Tennis (Women)",
-        "Squash (Men)",
-        "Squash (Women)",
+        "Squash",
         "8 Ball Pool (Mixed)",
         "Snooker (Mixed)",
         "Shooting",
@@ -244,8 +242,7 @@ export const SportsGuidlines: Record<string, string> = {
     Volleyball_Women: "volleyball",
     Table_Tennis_Men: "tabletennis",
     Table_Tennis_Women: "tabletennis",
-    Squash_Men: "squash",
-    Squash_Women: "squash",
+    Squash: "squash",
     Swimming_Men: "swimming",
     Swimming_Women: "swimming",
     Ball_Pool_Mixed: "pool",
@@ -268,13 +265,13 @@ export const eventSchema: EventSchema = {
         Badminton_Men: {
             eventName: sports.Badminton_Men,
             specificPages: [
-                generatePageWithPlayerFields(5, 7), // 5 to 7 players
+                generatePageWithPlayerFields(4, 7), // 5 to 7 players
             ],
         },
         Badminton_Women: {
             eventName: sports.Badminton_Women,
             specificPages: [
-                generatePageWithPlayerFields(5, 7), // 5 to 7 players
+                generatePageWithPlayerFields(4, 7), // 5 to 7 players
             ],
         },
         Basketball_Men: {
@@ -331,16 +328,28 @@ export const eventSchema: EventSchema = {
                 generatePageWithPlayerFields(4, 5), // 3 to 5 players
             ],
         },
-        Squash_Men: {
-            eventName: sports.Squash_Men,
+        Squash: {
+            eventName: sports.Squash,
             specificPages: [
-                generatePageWithPlayerFields(3, 5), // 5 players
-            ],
-        },
-        Squash_Women: {
-            eventName: sports.Squash_Women,
-            specificPages: [
-                generatePageWithPlayerFields(3, 5), // 5 players
+                {
+                    pageName: "Coach Details",
+                    fields: z.object({
+                        coachFields,
+                        playerFields: z.array(playerFields.extend({ gender: z.enum(["Select Gender", "Male", "Female"], { message: "Gender is required" }) }))
+                            .min(1, `Fill details of minimum ${1} players`)
+                            .max(10, `A maximum of ${10} players are allowed`),
+                    }),
+                    draft: z.object({
+                        coachFields,
+                        playerFields: z.array(playerFieldsDraft.extend({ gender: z.enum(["Select Gender", "Male", "Female"], { message: "Gender is required" }) }))
+                            .min(1, `Fill details of minimum ${1} players`)
+                            .max(10, `A maximum of ${10} players are allowed`),
+                    }),
+                    meta: {
+                        coachFields: coachFieldsMeta,
+                        playerFields: playerMeta,
+                    },
+                }
             ],
         },
         Ball_Pool_Mixed: {
